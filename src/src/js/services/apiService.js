c70b57209c104fa8965d9463bd8adab98fbffdac
i1-kmv <i1-kmv@mail.ru>
2020-12-28 22:12:55 +0300

    step 4

--- src/src/js/services/apiService.js
+++ src/src/js/services/apiService.js
@@ -28,17 +28,7 @@ class Api {
       return Promise.reject(err);
     }
   }
-  async prices(params) {
-    try {
-      const response = await axios.get(`${this.url}/prices/cheap`, {
-        params,
-      });
-      return response.data;
-    } catch (err) {
-      console.log(err);
-      return Promise.reject(err);
-    }
-  }
+  async prices(params) {}
 }
 
 const api = new Api(config);
